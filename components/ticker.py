9520da4272be98ad9097fa44ebc4d199749923c5
Krittitee Chaisang <kongbeng21@gmail.com>
2025-12-11 13:18:56 +0700

    add BidAskCard in ticker

--- components/ticker.py
+++ components/ticker.py
@@ -10,3 +10,33 @@ class PriceCard(tk.Frame):
     def update_data(self, price, change):
         color = config.GREEN_COLOR if float(change) >= 0 else config.RED_COLOR
         self.price_label.config(text=f"${float(price):,.2f}", fg=color)
+
+class BidAskCard(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent, bg=config.COMPONENT_BG)
+        title = tk.Label(self, text="Best Bid / Ask & Spread", 
+                         bg=config.COMPONENT_BG, fg=config.TEXT_COLOR, font=config.FONT_MAIN)
+        title.pack(pady=5)
+        self.content_frame = tk.Frame(self, bg=config.COMPONENT_BG)
+        self.content_frame.pack(expand=True)
+        
+        self.bid_label = tk.Label(self.content_frame, text="Bid: --", 
+                                  bg=config.COMPONENT_BG, fg=config.GREEN_COLOR, font=config.FONT_MAIN)
+        self.bid_label.pack(anchor="w")
+        
+        self.ask_label = tk.Label(self.content_frame, text="Ask: --", 
+                                  bg=config.COMPONENT_BG, fg=config.RED_COLOR, font=config.FONT_MAIN)
+        self.ask_label.pack(anchor="w")
+        
+        self.spread_label = tk.Label(self.content_frame, text="Spread: --", 
+                                     bg=config.COMPONENT_BG, fg="orange", font=config.FONT_MAIN)
+        self.spread_label.pack(anchor="w")
+
+    def update_data(self, bid, ask):
+        bid_float = float(bid)
+        ask_float = float(ask)
+        spread = ask_float - bid_float
+        
+        self.bid_label.config(text=f"BID(Buy): {bid_float:,.2f}")
+        self.ask_label.config(text=f"ASK(Sell): {ask_float:,.2f}")
+        self.spread_label.config(text=f"Spread: {spread:.4f}")
