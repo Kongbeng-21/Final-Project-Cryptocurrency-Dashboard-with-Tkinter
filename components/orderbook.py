87c61a8e12a934e9b0d7474412a7a542c2238a1c
Krittitee Chaisang <kongbeng21@gmail.com>
2025-12-13 00:21:01 +0700

    change ws_url to supported other coins

--- components/orderbook.py
+++ components/orderbook.py
@@ -6,10 +6,9 @@ class OrderBook(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent, bg=config.COMPONENT_BG)
         
-        header = tk.Label(self, text="Order Book (Depth 10)", 
+        header = tk.Label(self, text="Order Book", 
                           bg=config.COMPONENT_BG, fg=config.TEXT_COLOR, font=config.FONT_MAIN)
         header.pack(pady=5)
-
         columns = ("price", "amount")
         self.tree = ttk.Treeview(self, columns=columns, show="headings", height=10)
         self.tree.heading("price", text="Price (USDT)")
@@ -23,7 +22,6 @@ class OrderBook(tk.Frame):
     def update_data(self, bids, asks):
         for item in self.tree.get_children():
             self.tree.delete(item)
-            
         for price, amount in reversed(asks[:5]): 
             self.tree.insert("", "end", values=(f"{float(price):,.2f}", f"{float(amount):.5f}"), tags=("ask",))
         self.tree.insert("", "end", values=("---", "---"))
